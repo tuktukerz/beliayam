509689636a25a29b27cba34a6af419b8c8bba9e2
astrsm <ddefrito84@gmail.com>
2024-01-16 18:13:27 +0700

    style(welcome): change black logo to light background

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -324,7 +324,7 @@ $loopCount = ceil(count($customers) / 4);
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse mb-4">
-                <img src="{{ asset('storage/logo/' . $identities->logo_light) }}" class="h-12"
+                <img src="{{ asset('storage/logo/' . $identities->logo_black) }}" class="h-12"
                      alt="Beli Ayam Logo"/>
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
