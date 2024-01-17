c88d094101cae2c626d3f14fd7b98a3268add2c7
astrsm <ddefrito84@gmail.com>
2024-01-17 14:58:18 +0700

    style: active navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -21,15 +21,8 @@
         </div>
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
             <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                    <a href="/"
-                       class="block py-2 px-3 md:p-0 text-white bg-red-700 rounded md:bg-transparent md:text-red-700 md:dark:text-red-500 hover:text-white hover:bg-red-800 transition"
-                       aria-current="page">Home</a>
-                </li>
-                <li>
-                    <a href="/product"
-                       class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 transition">Product</a>
-                </li>
+                <x-navlink text="home" />
+                <x-navlink text="product" />
             </ul>
         </div>
     </div>
--- resources/views/components/navlink.blade.php
+++ resources/views/components/navlink.blade.php
@@ -1,9 +1,9 @@
 <li>
     @php
         $pathname = '/' . ($text == 'home' ? '' : $text);
-        $isActive = Request::is($pathname);
-        $activeClass = 'block py-2 px-3 text-white bg-yellow-300 rounded md:bg-transparent md:text-yellow-300 md:p-0  md:py-6 md:border-b-8 md:border-yellow-300 md:rounded-none';
-        $nonActiveClass = 'block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-300 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:py-6 ';
+        $isActive = Request::path() == ($text == 'home' ? '/' : $text);
+        $activeClass = 'block py-2 px-3 md:p-0 text-white bg-red-700 hover:bg-red-800 md:hover:bg-transparent rounded md:bg-transparent md:text-red-700 md:dark:text-red-500 md:hover:text-red-800 transition';
+        $nonActiveClass = 'block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-red-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 transition';
     @endphp
     <a href={{ $pathname }} @class([
         $activeClass => $isActive,
