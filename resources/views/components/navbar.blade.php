061a402b199d310a61d243ab882c665bca7b0941
astrsm <ddefrito84@gmail.com>
2024-01-26 00:16:52 +0700

    refactor: navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -21,8 +21,10 @@
         </div>
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
             <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <x-navlink text="home" />
-                <x-navlink text="product" />
+                <x-navlink text="home" link="/"/>
+                <x-navlink text="product" link="product"/>
+                <x-navlink text="about" link="about"/>
+                <x-navlink text="order" link="about#form_order"/>
             </ul>
         </div>
     </div>
