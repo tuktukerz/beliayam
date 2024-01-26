4f476009a9fb86369e265fcc2f8bea1d2ded8514
tuktukerz <aflianmahyaruddin@gmail.com>
2024-01-27 02:48:19 +0700

    refactor(Content) : refactor content, fix validation & return with message

--- resources/views/layouts/contentView/index.blade.php
+++ resources/views/layouts/contentView/index.blade.php
@@ -4,13 +4,20 @@
 @section('content')
 <div id="main-content">
     <div class="page-heading">
+        @if(session('success'))
+        <div class="alert alert-success alert-dismissible show fade">
+            {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        @endif
         <div class="page-title">
             <div class="row">
                 <div class="col-12 col-md-6 order-md-1 order-last">
-                    <h3>Semua Content</h3>
+                    <h3>Semua Konten</h3>
                     <p class="text-subtitle text-muted">
-                        Berikut adalah semua list content.
+                        Berikut adalah semua list konten.
                     </p>
+
                     <a href="{{ route('content.create') }}" class="btn icon icon-left btn-primary mb-4">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                              fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
@@ -19,7 +26,7 @@
                             <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z">
                             </path>
                         </svg>
-                        Tambah Content
+                        Tambah Konten
                     </a>
                 </div>
                 <div class="col-12 col-md-6 order-md-2 order-first">
